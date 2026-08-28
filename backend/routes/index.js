ffb7c528fe17f56dcfc24a9af1b6968a8b04d073
Ahmed A Hassan <111147940+ahmed-dalab@users.noreply.github.com>
2026-08-28 20:26:37 +0300

    Revert "fet:Creating frontdoor page and installing backend and connect database"

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthRouter from "./auth.routes.js";
-
-const CentralRouter = Router();
-
-CentralRouter.use('/auth', AuthRouter);
-
-export default CentralRouter;
-
